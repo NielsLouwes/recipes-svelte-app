311a7f937067473dc3ebb965aef709b60c0ba8f8
Niels Louwes <nielslouwes45@gmail.com>
2024-05-31 07:53:14 +0200

    project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import recipes from '../data/recipes.json';
+
+	const recipesData = recipes['recipes'];
+	console.log('recipesData', recipesData);
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Recipe Shopping App" />
+</svelte:head>
+
+<section>
+	<p>{recipesData.length}</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/shopping-list/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Shopping List</title>
+	<meta name="shopping-list" content="Shopping list" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Shopping List</h1>
+
+	<p>
+		Here will be the shopping list. Need to implement adding items from the recipes to the shopping
+		list.
+	</p>
+</div>
